eed69e4bd2759a8f68c973d0f55cd223a8e0121c
Parth Shah <parthhshah171@gmail.com>
2026-03-01 08:54:17 +0000

    remove python setup_complete, switching to go

--- workload/entrypoint/setup_complete.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env -S python3 -u
-
-from antithesis.lifecycle import (
-    setup_complete,
-)
-
-setup_complete({"Message":"Lotus-1 has reached blockheight 10"})
-print("system is healthy")
